bf861fefaa50cef822f36a42a6d566c3a594f0c0
zxren <zxren@trip.com>
2020-12-22 23:28:08 +0800

    init java web

---
+++ README.md
@@ -0,0 +1,4 @@
+## 项目构建
+1. 到start.spring.io构建一个基础项目，只需包含lombok和spring web即可
+2. Idea中打开项目，右键WebAdminApplication运行，如果程序运行正常且打开localhost:8080看到正常的白页说明项目正常运行了。
+3. 新建文件.gitignore,将.DS_Store .idea/ target/加入到忽略文件中
---
+++ src/test/java/com/nuptaxin/webadmin/WebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuptaxin.webadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
